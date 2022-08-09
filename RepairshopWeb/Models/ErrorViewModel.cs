e765145111560939194acb452be39f72f670da50
beeavelar <avelar.dbr@gmail.com>
2022-08-09 16:48:13 -0300

    Add project files.

---
+++ RepairshopWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RepairshopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
